a0036ddd8db75753577750b5616bb92e6acb627a
rai8 <hytonne@outlook.com>
2019-09-09 23:42:50 +0300

    mapping state to props using connect from the react-redux

--- src/components/store/reducers/projectReducers.js
+++ src/components/store/reducers/projectReducers.js
@@ -1,4 +1,22 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "React-redux",
+      content: "read on the react redux library, store and its reducers"
+    },
+    {
+      id: "2",
+      title: "Routing",
+      content: "read on the react router dom and how to use all child elements"
+    },
+    {
+      id: "3",
+      title: "Express js",
+      content: "read on the amazing web framework incoporating node and mongodb"
+    }
+  ]
+};
 
 const projectReducers = (state = initState, action) => {
   return state;
